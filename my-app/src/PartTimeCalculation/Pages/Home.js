68855f548bd4613bf2d4dc643d8cd2f776721370
Zl존기원 <gkgk7610@naver.com>
2024-03-05 17:48:10 +0900

    npm install 안한게 많네..? ㅎ다 다운받고 달력도 일단 만들어 봤는데 좀더 손 봐야 할거같음

--- my-app/src/PartTimeCalculation/Pages/Home.js
+++ my-app/src/PartTimeCalculation/Pages/Home.js
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Home = () => {
+		const [currentDate, setCurrentDate] = useState(new Date());
+		const [year, setYear] = useState(2024);
+		const [month, setMonth] = useState(1); // 월은 0부터 시작
+
+		useEffect(() => {
+				// 현재 날짜 표시
+				const currentDateElement = document.getElementById("currentDate");
+				currentDateElement.innerText = "현재 날짜: " + currentDate.toLocaleString();
+
+				// 달력 생성
+				const calendarTable = document.getElementById("calendarTable");
+				const firstDay = new Date(year, month, 1);
+				const lastDay = new Date(year, month + 1, 0);
+
+				const headerRow = calendarTable.insertRow(0);
+				const daysOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
+
+				for (let i = 0; i < 7; i++) {
+						const cell = headerRow.insertCell(i);
+						cell.innerText = daysOfWeek[i];
+				}
+
+				let currentDate2 = new Date(firstDay);
+				let currentRow = calendarTable.insertRow();
+
+				// 시작 요일까지 빈 셀 추가
+				for (let i = 0; i < firstDay.getDay(); i++) {
+						const cell = currentRow.insertCell(i);
+						cell.innerText = "";
+				}
+
+				// 날짜 채우기
+				while (currentDate2.getMonth() === month) {
+						if (currentDate2.getDay() === 0) {
+								currentRow = calendarTable.insertRow();
+						}
+
+						const cell = currentRow.insertCell(currentDate2.getDay());
+						cell.innerText = currentDate2.getDate();
+
+						currentDate2.setDate(currentDate2.getDate() + 1);
+				}
+		}, [currentDate, year, month]);
+
 		return (
 				<div>
-						이거시 메인 페이지가 될거시여
-						이제 진짜 올라감 ㄹㅇ
+						<div id="currentDate">현재 날짜</div>
+						<table id="calendarTable"></table>
 				</div>
 		);
 };
